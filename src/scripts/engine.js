44618996a3fc6300113a94512dc5a3b3bcb33f11
rsbastos <renedossantosbastos@gmail.com>
2024-02-20 21:53:30 -0300

    :sparkle: feat: card functions - css styles

---
+++ src/scripts/engine.js
@@ -0,0 +1,44 @@
+const emojis = [
+    "🐶",
+    "🐶",
+    "😺",
+    "😺",
+    "🐭",
+    "🐭",
+    "🐷",
+    "🐷",
+    "🐮",
+    "🐮",
+    "🐼",
+    "🐼",
+    "🐴",
+    "🐴",
+    "🐵",
+    "🐵"
+];
+let openCards = [];
+
+let shuffleEmojis = emojis.sort(() => (Math.random() > 0.5 ? 2 : -1));
+
+for(let i=0; i < emojis.length; i++) {
+    let box = document.createElement("div");
+    box.className = "item";
+    box.innerHTML = shuffleEmojis[i];
+    box.onclick = handleClick;
+    document.querySelector(".game").appendChild(box);
+}
+
+function handleClick() {
+    if(openCards.length < 2) {
+        this.classList.add("boxOpen");
+        openCards.push(this);
+    }
+
+    if(openCards.length == 2) {
+        setTimeout(checkMatch, 500);
+    }
+}
+
+function checkMatch() {
+    
+}
